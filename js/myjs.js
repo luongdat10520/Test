0ee72e35c7930c9647ea80400615b256d0364c67
luongdat10520 <luongdat246@gmail.com>
2021-12-07 20:59:54 +0700

    test

---
+++ js/myjs.js
@@ -0,0 +1,38 @@
+function getOption1() {
+    let question1 = document.getElementById("question1").value;
+    output1 = selectElement.value, node1;
+    for ( var i = nodes.length; i--; ) {
+        node1 = nodes[i];  
+    }
+}
+function getOption2() {
+    let question2 = document.getElementById("question2").value;
+    output2 = selectElement.value, node2;
+    for ( var j = nodes.length; j--; ) {
+        node2 = nodes[j];  
+    }
+}
+function getOption3() {
+    let question3 = document.getElementById("question3").value;
+    output3 = selectElement.value, node3;
+    for ( var k = nodes.length; k--; ) {
+        node3 = nodes[k];  
+    }
+}
+let btnsubmit = document.getElementById("btnsubmit");
+let bltitlet = document.getElementById("bltitlet");
+let imgaswer = document.getElementById("imgaswer");
+
+imgaswer.addEventListener=("click", changeimg);
+
+function changeimg() {
+    let choose1 = node1.value;
+    let choose2 = node2.value;
+    let choose3 = node3.value;
+
+
+    if(node1=nodes[1]; node2=nodes[3]; node3=nodes[2]) {
+        
+    }
+  
+}
